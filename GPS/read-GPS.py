8e655928f53803d572ea5241912e8bee6224a2e4
kipod <nchernov@gmail.com>
2023-12-26 14:24:42 +0200

    GPS

---
+++ GPS/read-GPS.py
@@ -0,0 +1,18 @@
+import serial
+import pynmea2
+
+ser = serial.Serial('/dev/ttyS0', baudrate=9600, timeout=1)
+
+try:
+    while True:
+        data = ser.readline().decode('utf-8')
+
+        if data.startswith('$GPGGA'):
+            msg = pynmea2.parse(data)
+            latitude = "{:.6f}".format(msg.latitude)
+            longitude = "{:.6f}".format(msg.longitude)
+
+            print(f"{latitude}, {longitude}")
+
+except KeyboardInterrupt:
+    ser.close()
