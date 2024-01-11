e44489b29e88b0abcef3c55c3b3b967d389f207c
Mykola <nchernov@gmail.com>
2024-01-11 15:59:08 +0200

    tcp server

--- tcp_server.py
+++ tcp_server.py
@@ -1,20 +1,70 @@
+import struct
 import asyncio
+from asyncio import StreamReader, StreamWriter
+from datetime import datetime
 
+from qu import Queue, QData
 from config import Settings, get_settings
 from logger import log
 
 SETTINGS: Settings = get_settings()
+BUFF_SIZE = 36
+
+queue = Queue()
 
 
-async def handle_client(reader, writer):
-    ...  # handle client here
+async def handle_client(reader: StreamReader, writer: StreamWriter):
+    log(log.INFO, "TCP Client connected")
+
+    DELTA_TIME = 2  # seconds
+    prev_counter = 0
+    start_counter = 0
+    start = datetime.now()
+    while True:
+        data_byte = await reader.read(BUFF_SIZE)
+        if not data_byte:
+            break
+        assert len(data_byte) == BUFF_SIZE
+        counter, *values = struct.unpack("I16H", data_byte[:4])
+        if prev_counter:
+            assert counter - prev_counter == 1
+        prev_counter = counter
+        now = datetime.now()
+        time_delta = now - start
+        if time_delta.seconds >= DELTA_TIME:
+            start = now
+            log(log.INFO, "speed: %d", (counter - start_counter) / DELTA_TIME)
+            start_counter = counter
+
+        # write data to redis
+        qdata = QData(
+            value1=values[0],
+            value2=values[1],
+            value3=values[2],
+            value4=values[3],
+            value5=values[4],
+            value6=values[5],
+            value7=values[6],
+            value8=values[7],
+            value9=values[8],
+            value10=values[9],
+            value11=values[10],
+            value12=values[11],
+            value13=values[12],
+            value14=values[13],
+            value15=values[14],
+            value16=values[15],
+        )
+        queue.push(qdata)
+
+    log(log.INFO, "TCP Client disconnected")
+    writer.close()
 
 
 async def main():
     server = await asyncio.start_server(handle_client, "0.0.0.0", SETTINGS.TCP_SERVER_PORT)
 
     addr = server.sockets[0].getsockname()
-    # print(f'Server started on {addr}')
     log(log.INFO, "Server started on [%s]", addr)
 
     async with server:
